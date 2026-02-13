d9d7ceb1da367d5a83c533dcb349348761b9c4b7
Jay Te <jayte@Jays-MacBook-Pro.local>
2026-02-13 13:41:49 +0800

    feat(mobile): setup supabase client and dependencies

---
+++ lib/supabase.ts
@@ -0,0 +1,29 @@
+import { AppState } from 'react-native';
+import 'react-native-url-polyfill/auto';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL || '';
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || '';
+
+if (!supabaseUrl || !supabaseAnonKey) {
+    console.warn('Supabase URL or Key is missing from environment variables.');
+}
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+    auth: {
+        storage: AsyncStorage,
+        autoRefreshToken: true,
+        persistSession: true,
+        detectSessionInUrl: false,
+    },
+});
+
+// Instruct Supabase to auto-refresh only when app is in foreground
+AppState.addEventListener('change', (state) => {
+    if (state === 'active') {
+        supabase.auth.startAutoRefresh();
+    } else {
+        supabase.auth.stopAutoRefresh();
+    }
+});
